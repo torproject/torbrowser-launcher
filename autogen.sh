f4c87aa5de188d4a2dfcedb8394bbe95a9c9e9ab
Micah Lee <micahflee@riseup.net>
2013-02-10 13:45:18 -0800

    testing to switch from python gtk to javascript gtk

---
+++ autogen.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd "$srcdir"
+
+# This will run autoconf, automake, etc. for us
+autoreconf --force --install
+
+cd "$olddir"
+
+if test -z "$NOCONFIGURE"; then
+  "$srcdir"/configure "$@"
+fi
