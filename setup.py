022416d2cc446bcba50aa3b99b9bb6f301a3bb93
Micah Lee <micahflee@riseup.net>
2013-02-14 18:18:17 -0800

    back to python

--- setup.py
+++ setup.py
@@ -1,38 +1,23 @@
-Tor Browser Launcher
-====================
+from distutils.core import setup
 
+setup(name='torbrowser-launcher',
+      version='0.1',
+      author='Micah Lee',
+      author_email='micahflee@riseup.net',
+      url='https://www.github.com/micahflee/torbrowser-launcher',
+      platforms=['GNU/Linux'],
+      license='GPLv3',
+
+      description='A program to help you download, keep updated, and run the Tor Browser Bundle',
+      long_description="""
 Tor Browser Launcher is intended to make the Tor Browser Bundle (TBB) easier to maintain and use for GNU/Linux users. You install torbrowser-launcher from your distribution's package manager and it handles downloading the most recent version of TBB for you, in your language and for your architecture. It also adds a "Tor Browser" application launcher to your operating system's menu.
 
 When you first launch Tor Browser Launcher, it will download TBB from https://www.torproject.org/ and extract it in ~/.torproject, and then execute it. When you run it after that it will just execute TBB.
 
 Tor Browser Launcher will get updated each time a new version of TBB is released. When you open Tor Browser after an update, it will download the newer version of TBB for you and extract it over your old TBB directory in ~/.torproject, so you will maintain your TBB bookmarks. 
+""",
 
-Building a Debian package
-=========================
-
-You need to have stdeb installed. If you're using Debian or Ubuntu you can install it like this:
-
-  sudo apt-get install python-stdeb
-
-Here are other things you might need:
-
-  sudo apt-get install build-essential dpkg-dev debhelper devscripts fakeroot cdbs
-
-First build a source distribution:
-
-  python setup.py sdist
-
-Turn it into a Debian source package:
-
-  cd dist
-  py2dsc torbrowser-launcher-2.3.25-2-1.tar.gz
-
-Turn it into a Debian binary package:
-
-  cd deb_dist/torbrowser-launcher-2.3.25-2-1
-  dpkg-buildpackage -rfakeroot -uc -us
-
-Once you've made a .deb, you can install it like this:
-
-  sudo dpkg -i python-torbrowser-launcher_0.1_all.deb
-
+      scripts=['torbrowser-launcher'],
+      data_files=[('/usr/share/applications', ['misc/torbrowser.desktop']),
+                  ('/usr/share/pixmaps', ['misc/torbrowser32.xpm', 'misc/torbrowser80.xpm'])]
+      )
