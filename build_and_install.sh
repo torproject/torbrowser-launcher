9143df5a40a19d5a40381b1a3b8101a9aa00fd51
Micah Lee <micahflee@riseup.net>
2013-03-02 20:57:32 -0800

    added stdeb.cfg, so the debian package name is torbrowser-launcher instead of python-torbrowser-launcher, and it also contains the correct debian dependencies (including tor). fixes #5.

--- build_and_install.sh
+++ build_and_install.sh
@@ -3,19 +3,9 @@
 # clean up from last time
 rm -rf ~/.torbrowser
 
-# build source dist
-python setup.py sdist
-
-# turn it into a debian source package
-cd dist
-py2dsc torbrowser-launcher-0.1.tar.gz
-
-# turn it into a debian binary package
-cd deb_dist/torbrowser-launcher-0.1
-dpkg-buildpackage -rfakeroot -uc -us
+# build binary package
+python setup.py --command-packages=stdeb.command bdist_deb
 
 # install it
-cd ..
-sudo dpkg -i python-torbrowser-launcher_0.1-1_all.deb
-cd ../..
+sudo dpkg -i deb_dist/torbrowser-launcher_0.1-1_all.deb
 
