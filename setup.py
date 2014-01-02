644c8c3755dfc878ec6b1bd0f0a215022505b58d
Micah Lee <micah@micahflee.com>
2014-01-02 15:55:30 -0800

    wrote apparmor profile for torbrowser-launcher itself (#72)

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ Tor Browser Launcher will get updated each time a new version of TBB is released
                   ('/usr/share/pixmaps', ['img/torbrowser32.xpm', 'img/torbrowser80.xpm']),
                   ('/usr/share/torbrowser-launcher', ['keys/erinn.asc', 'keys/sebastian.asc', 'keys/alexandre.asc', 'keys/mike.asc', 'keys/mike-2013-09.asc', 'torproject.pem', 'mirrors.txt', 'modem.ogg']),
                   ('/usr/share/torbrowser-launcher/locale/en', ['locale/en/messages.pot']),
-                  ('/etc/apparmor.d/', ['apparmor/torbrowser.Browser.firefox', 'apparmor/torbrowser.start-tor-browser', 'apparmor/torbrowser.Tor.tor']),
+                  ('/etc/apparmor.d/', ['apparmor/torbrowser.Browser.firefox', 'apparmor/torbrowser.start-tor-browser', 'apparmor/torbrowser.Tor.tor', 'apparmor/usr.bin.torbrowser-launcher']),
 
                   # unpackaged third party libraries
                   ('/usr/share/torbrowser-launcher/lib/txsocksx', file_list('lib/txsocksx-0.0.2/txsocksx')),
