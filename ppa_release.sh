a421451af4fa94f22a91da82dc770906301ec8ec
Micah Lee <micah@micahflee.com>
2017-01-26 20:09:08 -0800

    Add more verbose comments to ppa release script

--- ppa_release.sh
+++ ppa_release.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
 # This script pushes updates to my Ubuntu PPA: https://launchpad.net/~micahflee/+archive/ppa
-# If you want to use it, you'll need your own ~/.dput.cf and ssh key.
 # More info: https://help.launchpad.net/Packaging/PPA/Uploading
+#
+# If you want to use it, you'll need your own ~/.dput.cf. Here's mine:
+#
+# [ppa]
+# fqdn = ppa.launchpad.net
+# method = ftp
+# incoming = ~micahflee/ubuntu/ppa/
+# login = anonymous
+# allow_unsigned_uploads = 0
 
 VERSION=`cat share/torbrowser-launcher/version`
 
+# Make a source pacakge
 rm -rf deb_dist
 python setup.py --command-packages=stdeb.command sdist_dsc
+
+# Sign it
 cd deb_dist/torbrowser-launcher-$VERSION
 dpkg-buildpackage -S
+#dpkg-buildpackage -S -pqubes-gpg-client-wrapper -k927F419D7EC82C2F149C1BD1403C2657CD994F73
 cd ..
-dput ppa:micahflee/ppa torbrowser-launcher_$VERSION-1_source.changes
+
+# Push it to the ppa
+dput ppa torbrowser-launcher_$VERSION-1_source.changes
 cd ..
