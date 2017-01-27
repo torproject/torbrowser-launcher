cc5c22e8a882c0a6e17eedd4a137e86f1b255ba5
Micah Lee <micah@micahflee.com>
2017-01-26 20:25:49 -0800

    Update copyright year to 2017

--- torbrowser_launcher/__init__.py
+++ torbrowser_launcher/__init__.py
@@ -2,7 +2,7 @@
 Tor Browser Launcher
 https://github.com/micahflee/torbrowser-launcher/
 
-Copyright (c) 2013-2014 Micah Lee <micah@micahflee.com>
+Copyright (c) 2013-2017 Micah Lee <micah@micahflee.com>
 
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
@@ -63,4 +63,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
