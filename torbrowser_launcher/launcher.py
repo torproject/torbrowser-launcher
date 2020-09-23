de73d5363607200ba207f0c21d74e0cb89b5d66f
mentha <mentha@users.noreply.github.com>
2020-09-23 16:03:14 +0800

    Fix #462 DNS leak when “downloading over tor”

--- torbrowser_launcher/launcher.py
+++ torbrowser_launcher/launcher.py
@@ -489,7 +489,7 @@ class DownloadThread(QtCore.QThread):
 
         # Use tor socks5 proxy, if enabled
         if self.common.settings['download_over_tor']:
-            socks5_address = 'socks5://{}'.format(self.common.settings['tor_socks_address'])
+            socks5_address = 'socks5h://{}'.format(self.common.settings['tor_socks_address'])
             self.proxies = {
                 'https': socks5_address,
                 'http': socks5_address
