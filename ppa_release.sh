adb51f6904fbdc874bb058525fd6f3a3056beae8
a1346054 <36859588+a1346054@users.noreply.github.com>
2021-08-22 20:48:55 +0000

    Fix shellcheck warnings

--- ppa_release.sh
+++ ppa_release.sh
@@ -12,18 +12,18 @@
 # login = anonymous
 # allow_unsigned_uploads = 0
 
-VERSION=`cat share/torbrowser-launcher/version`
+VERSION=$(cat share/torbrowser-launcher/version)
 
 # Make a source pacakge
 rm -rf deb_dist
 python3 setup.py --command-packages=stdeb.command sdist_dsc
 
 # Sign it
-cd deb_dist/torbrowser-launcher-$VERSION
+cd "deb_dist/torbrowser-launcher-$VERSION"
 dpkg-buildpackage -S
 #dpkg-buildpackage -S -pqubes-gpg-client-wrapper -k927F419D7EC82C2F149C1BD1403C2657CD994F73
 cd ..
 
 # Push it to the ppa
-dput ppa torbrowser-launcher_$VERSION-1_source.changes
+dput ppa "torbrowser-launcher_$VERSION-1_source.changes"
 cd ..
