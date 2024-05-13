72b7d24423635bc43d0f87e5bb1f1ebc010fb5f5
Denis Laxalde <denis@laxalde.org>
2024-05-13 10:21:41 +0200

    Use Browser/start-tor-browser instead of .desktop file
    This should have no functional consequence but will help with allowing
    to start the browser with non-default options.

--- torbrowser_launcher/launcher.py
+++ torbrowser_launcher/launcher.py
@@ -478,7 +478,8 @@ class Launcher(QtWidgets.QMainWindow):
 
         # Run Tor Browser
         subprocess.call(
-            [self.common.paths["tbb"]["start"]], cwd=self.common.paths["tbb"]["dir_tbb"]
+            [self.common.paths["tbb"]["start"], "--detach"],
+            cwd=self.common.paths["tbb"]["dir_tbb"],
         )
         sys.exit(0)
 
